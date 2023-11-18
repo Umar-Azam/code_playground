21a83e083015531101b6a2c04d3165f3a9b035c1
Umar-Azam <92691687+Umar-Azam@users.noreply.github.com>
2023-11-18 23:12:29 +0000

    better naming

--- gpt-crawler-image/run.sh
+++ gpt-crawler-image/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Check if there is a Docker image named "minimal"
-if ! sudo docker images | grep -w 'minimal' > /dev/null; then
-    echo "Docker repository 'minimal' not found. Building the image..."
-    # Build the Docker image with the name 'minimal'
-    sudo docker build -t minimal .
+# Check if there is a Docker image named "crawler"
+if ! sudo docker images | grep -w 'crawler' > /dev/null; then
+    echo "Docker repository 'crawler' not found. Building the image..."
+    # Build the Docker image with the name 'crawler'
+    sudo docker build -t crawler .
 else
     echo "Docker image already built."
 fi
 
-sudo docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock minimal bash
+sudo docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock crawler bash
